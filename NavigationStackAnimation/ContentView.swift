e157c0c72e922ad30c3f8e4e0c6e8deba0c69730
Aoitoridayo <aoitoridayo1110@icloud.com>
2024-05-09 17:56:09 +0900

    commit

--- NavigationStackAnimation/ContentView.swift
+++ NavigationStackAnimation/ContentView.swift
@@ -7,15 +7,119 @@
 
 import SwiftUI
 
+enum PreScanPath: Hashable {
+    case step1
+    case step2
+    case step3
+    case step4
+    case video1
+    case video2
+    case video3
+}
+
 struct ContentView: View {
+    @State private var path: [PreScanPath] = []
+    var body: some View {
+        NavigationStack(path: $path) {
+            Button("goto step1") {
+                path.append(PreScanPath.step1)
+            }
+            .navigationDestination(for: PreScanPath.self) { path in
+                switch path {
+                case .step1:
+                    Step1View(path: $path)
+                case .step2:
+                    Step2View(path: $path)
+                case .step3:
+                    Step3View(path: $path)
+                case .step4:
+                    Step4View(path: $path)
+                case .video1:
+                    Video1View(path: $path)
+                case .video2:
+                    Video2View(path: $path)
+                case .video3:
+                    Video3View(path: $path)
+                }
+            }
+        }
+    }
+}
+
+struct Step1View: View {
+    @Binding var path: [PreScanPath]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("goto step2") {
+                path.append(PreScanPath.step2)
+            }
         }
-        .padding()
+    }
+}
+
+struct Step2View: View {
+    @Binding var path: [PreScanPath]
+    var body: some View {
+        VStack {
+            Button("goto step3") {
+                path.append(PreScanPath.step3)
+            }
+        }
+    }
+}
+
+struct Step3View: View {
+    @Binding var path: [PreScanPath]
+    var body: some View {
+        VStack {
+            Button("goto step4") {
+                path.append(PreScanPath.step4)
+            }
+        }
+    }
+}
+
+struct Step4View: View {
+    @Binding var path: [PreScanPath]
+    var body: some View {
+        VStack {
+            Button("go back step1") {
+                path = [.step1]
+            }
+        }
+    }
+}
+
+struct Video1View: View {
+    @Binding var path: [PreScanPath]
+    var body: some View {
+        VStack {
+            Button("go to step2") {
+//                path.append(.step2)
+            }
+        }
+    }
+}
+
+struct Video2View: View {
+    @Binding var path: [PreScanPath]
+    var body: some View {
+        VStack {
+            Button("go to step2") {
+//                path.append(.step2)
+            }
+        }
+    }
+}
+
+struct Video3View: View {
+    @Binding var path: [PreScanPath]
+    var body: some View {
+        VStack {
+            Button("go to step2") {
+//                path = []
+            }
+        }
     }
 }
 
