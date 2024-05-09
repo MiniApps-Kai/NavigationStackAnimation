d092e29e2c3116dfeb067d94ac6a3085abd33356
Aoitoridayo <aoitoridayo1110@icloud.com>
2024-05-09 11:35:54 +0900

    Initial Commit

---
+++ NavigationStackAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationStackAnimation
+//
+//  Created by 渡邊魁優 on 2024/05/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationStackAnimation/NavigationStackAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackAnimationApp.swift
+//  NavigationStackAnimation
+//
+//  Created by 渡邊魁優 on 2024/05/09.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
